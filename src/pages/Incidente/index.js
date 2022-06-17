03a6ecff7a50c488c6ca4cec3abd2c3c76a386f0
Jessica Castro <jessica.castros@outlook.com>
2022-06-16 22:21:18 -0300

    Página de incidentes realizada e já responsiva

--- src/pages/Incidente/index.js
+++ src/pages/Incidente/index.js
@@ -1,13 +1,21 @@
 import MenuIcon from '@mui/icons-material/Menu';
-import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import './styles.css'
 import { useEffect, useState } from 'react';
 import { database } from '../../services/firebase';
-import { collection, getDocs } from "firebase/firestore"
+import { collection, getDocs, addDoc } from "firebase/firestore"
+import { Link } from 'react-router-dom';
 
 const Incidente = () => {
   const [incidentes, setIncidentes] = useState([]);
+  const [incident, setIncident] = useState({});
   const [showModalAdd, setShowModalAdd] = useState(false);
+  const [showModalDetail, setShowModalDetail] = useState(false);
+
+  const [imageURL, setImageURL] = useState('');
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [location, setLocation] = useState('');
+  const [type, setType] = useState('queimada');
 
   useEffect(() => {
     getIncidentes();
@@ -18,13 +26,52 @@ const Incidente = () => {
 
     const docOcorrencias = await getDocs(ocorrenciasCollection);
  
-    const ocorrencias = docOcorrencias.docs.map(ocorrencia => ocorrencia.data());
+    const ocorrencias = docOcorrencias.docs.map(ocorrencia => {
+      const ocorrence = { 
+        id: ocorrencia.id,
+        ...ocorrencia.data() 
+      }
+
+      return ocorrence;
+    });
 
     setIncidentes(ocorrencias);
   }
 
-  const handleAddIncident = async () => {
+  const handleAddIncident = async (e) => {
+    e.preventDefault();
 
+    const data = {
+      author: 'Web User',
+      title,
+      description,
+      location,
+      type,
+      picture: imageURL
+    }
+
+    const ocorrenciasCollection = collection(database, "ocorrencias");
+
+    await addDoc(ocorrenciasCollection, data).then((response) => {
+      getIncidentes();
+      setShowModalAdd(false);
+      alert('Dados salvos com sucesso.')
+    });
+  }
+
+  const handleClickCard = (event, incident) => {
+    event.preventDefault();
+    setShowModalDetail(true);
+    setShowModalAdd(false);
+    setIncident(incident);
+    console.log(incident)
+  }
+
+
+  const handleModalClose = () => {
+    setShowModalAdd(false);
+    setShowModalDetail(false);
+    setIncident({});
   }
 
   return(
@@ -32,12 +79,12 @@ const Incidente = () => {
       <section className="incidente">
         <div className="header-incidente">
           <MenuIcon />
-          <FilterAltIcon />
+          <Link to="/profile">Perfil</Link>
         </div>
 
         <div className="wrapper-cards">
           {incidentes.length > 0 && incidentes.map(incidente => (
-              <div key={incidente.id} className="card">
+              <div onClick={e => handleClickCard(e, incidente)} key={incidente.id} className="card">
                 <img src={incidente.picture} alt="Foto do incidente" />
 
                 <div className="card-info">
@@ -47,18 +94,67 @@ const Incidente = () => {
               </div>
             ))}
         </div>
-        <button onClick={e => setShowModalAdd(true)} className="mais-incidentes">+</button>
+
+        <button onClick={e => setShowModalAdd(true)} className="more-incidents">+</button>
       </section>
 
       {
         showModalAdd && (
           <section className="modal-layer">
-            <div className="layer-header">
-              <button onClick={e => setShowModalAdd(false)}>x</button>
-            </div>
-
             <div className="modal">
-              
+              <div className="modal-header">
+              <h1>Novo incidente</h1>
+              <button onClick={handleModalClose}>x</button>
+              </div>
+              <form>
+                <input type="text" value={imageURL} onChange={e => setImageURL(e.target.value)} id="image" placeholder="URL da imagem" />
+
+                <input type="text" value={title} onChange={e => setTitle(e.target.value)} id="title" placeholder="Título" />
+
+                <select name="type" id="type" value={type} onChange={e => setType(e.target.value)}>
+                  <option value="queimada">Queimada</option>
+                  <option value="alagamento">Alagamento</option>
+                  <option value="animais">Animais abandonados</option>
+                  <option value="desperdicio">Desperdício de comida</option>
+                </select>
+
+                <input type="text" placeholder="Localização" value={location} onChange={e => setLocation(e.target.value)}/>
+
+                <textarea value={description} onChange={e => setDescription(e.target.value)} name="description" id="description" cols="30" rows="10" placeholder="Descrição"></textarea>
+
+                <button onClick={handleAddIncident}>Salvar</button>
+              </form>
+            </div>
+          </section>
+        )
+      }
+
+      {
+        showModalDetail && (
+          <section className="modal-layer">
+            <div className="modal">
+              <div className="modal-header">
+              <h1>Detalhe do incidente</h1>
+              <button onClick={handleModalClose}>x</button>
+              </div>
+              <form>
+                <img src={incident.picture} alt={incident.title}/>
+
+                <input type="text" value={incident.title} readOnly/>
+
+                <select name="type" id="type" value={incident.type} readOnly>
+                  <option value="queimada">Queimada</option>
+                  <option value="alagamento">Alagamento</option>
+                  <option value="animais">Animais abandonados</option>
+                  <option value="desperdicio">Desperdício de comida</option>
+                </select>
+
+                <input type="text" placeholder="Localização" value={incident.location} readOnly/>
+
+                <textarea readOnly value={incident.description} name="description" id="description" cols="30" rows="10" placeholder="Descrição"></textarea>
+
+                <button onClick={handleModalClose}>Fechar</button>
+              </form>
             </div>
           </section>
         )
